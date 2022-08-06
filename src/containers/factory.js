46990b975a87805cfb321caf44d2cff84b1da623
Juanma López <lopezjuanma96@gmail.com>
2022-08-06 19:41:13 -0300

    container factory

---
+++ src/containers/factory.js
@@ -0,0 +1,19 @@
+import {Container as Firebase} from './firebase.js';
+import {Container as Mongo} from './mongo.js';
+import {Container as Memory} from './memory.js';
+
+class ContainerFactory{
+    static getContainer(type, refer){
+        var container;
+        if (type === 'firebase') {
+            container = new Firebase(refer);
+        } else if (type === 'mongo') {
+            container = new Mongo(refer);
+        } else if (type === 'memory') {
+            container = new Memory();
+        }
+        return container;
+    }
+}
+
+return ContainerFactory;
