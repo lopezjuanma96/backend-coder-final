528180c949e9835edcd67450a5fb4084a31cb1d5
Juanma López <lopezjuanma96@gmail.com>
2022-08-10 09:31:40 -0300

    first layout chat basic hbs started with IO

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ import prodRouter from "./routes/products.js";
 
 //logger
 import logger from './utils/logger.js';
+
+//dao
+import chatDAO from './containers/daos/cartDAO.js';
+import chatDTO from './container/dtos/cartDTO.js';
 //----------- END IMPORTS --------------
 
 //------------- CONSTANTS ----------------
@@ -32,11 +36,11 @@ app.use(express.urlencoded({extended: true}))
 app.engine('hbs', engine({
     extname: '.hbs',
     defaultLayout: 'index.hbs',
-    layoutsDir: './views/layouts',
-    partialsDir: './views/partials'
+    layoutsDir: './src/views/layouts',
+    partialsDir: './src/views/partials'
 }))
 app.set('view engine', 'hbs');
-app.set('views', './views');
+app.set('views', './src/views');
 
 app.use(express.static('./public'))
 
@@ -46,4 +50,21 @@ app.use('/api/cart', cartRouter)
 
 http.listen(PORT, () => logger.info(`Server up in http://localhost:${PORT}`))
 
+io.on('connection', (socket) => {
+    logger.info(`New user connected!`);
+    chatDAO.getAll()
+    .then((msgs) => socket.emit('chatMessages', msgs))
+    .catch((err) => logger.error(err.message));
+    socket.on('newMessageInput', (msg) => {
+        chatDAO.save(new chatDTO({
+            //can w include id
+            user: msg.user,
+            text: msg.text,
+            date: Date.now()
+        }))
+        .then((saved) => {
+            socket.emit('newMessageUpdate', msg); //CHECK IF WE CAN DO THIS LIKE THIS OR RE-ASK FOR CHAT DAO FOR ALL MESSAGES
+        });
+    });
+})
 //----------- END SETUP --------------
