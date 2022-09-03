341be24a2727bd0d87a48e2eeac842e50942b167
Juanma López <lopezjuanma96@gmail.com>
2022-09-03 18:17:41 -0300

    added service for cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,41 +1,36 @@
-import CartDAO from '../containers/daos/cartDAO.js'
-import ProductsDAO from '../containers/daos/cartDAO.js'
-
-const cartDAO = new CartDAO();
-const productsDAO = new ProductsDAO();
+import {
+    getOneCartByUser,
+    fillCartProducts,
+    addProductToCart,
+    emptyCartProducts,
+    deleteProductFromCart
+} from '../services/cart.js'
 
 export async function getHandler(req){
     const user = req.session?.user || 'default';
-    const cart = await cartDAO.getFirst('user', user);
-    const cartProducts = []
-    var cartPrice;
-    for (id in cart.products){
-        const eachProd = await productsDAO.getFirst('id', id);
-        cartProducts.push({...eachProd.getForDb(), stock: cart.products[`${id}`]})
-        cartPrice += eachProd.getPrice() * cart.products[`${id}`];
-    }
+
+    const cart = await getOneCartByUser(user);
+    const { cartProducts, cartPrice } = await fillCartProducts(cart.products);
+    
     return { userData: {userAlias: user}, cartData: {items: cartProducts, exists: cartProducts.length > 0, total: cartPrice}}
 }
 
 export async function postHandler(req){
     const user = req.session?.user;
     const id = req.query.id;
-    const cart = await cartDAO.getFirst('user', user);
-    cart.addProduct(id);
-    await cartDAO.save(cart);
-    return cart.getForDb();
+
+    const data = await addProductToCart(user, id);
+
+    return data;
 }
 
 export async function deleteHandler (req, empty) {
     const user = req.session?.user;
-    const cart = await cartDAO.getFirst('user', user);
-    if (empty) {
-        cart.emptyProducts();
-        await cartDAO.save(cart)
-    } else {
-        const id = req.query.id;
-        cart.removeProduct(id);
-        await cartDAO.save(cart)
-    }
-    return cart.getForDb();
+    const id = req.query.id;
+    var data;
+
+    if (empty) data = await emptyCartProducts(user);
+    else if (id) data = await deleteProductFromCart(user, id);
+
+    return data;
 }
